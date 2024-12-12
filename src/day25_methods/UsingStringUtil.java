da1896046ae78bd2c9f19e65819274b14d2e77d7
dilovar85 <dilovar85@inbox.ru>
2024-12-11 22:07:37 -0500

    Reusable classes with custom methods

---
+++ src/day25_methods/UsingStringUtil.java
@@ -0,0 +1,19 @@
+package day25_methods;
+
+import my_utilities.StringUtil;
+
+public class UsingStringUtil {
+    public static void main(String[] args) {
+        //Arrays.sort();
+        System.out.println(StringUtil.fixFormat("jamES"));
+
+        System.out.println(StringUtil.frequencyOfLetters("loopcamp", 'o'));
+
+
+        System.out.println(StringUtil.reverse("loopcamp"));
+        System.out.println(StringUtil.duplicateCharacters("apple"));
+
+
+
+    }
+}
